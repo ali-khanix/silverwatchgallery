8a947b24b39dc37ed5ed83f2b2cc0017c0ef284e
Ali Khani <103037526+ali-khanix@users.noreply.github.com>
2025-10-09 16:29:53 +0330

    search input radius

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
       <Input
         placeholder={"جستجو..."}
         type={"search"}
-        className="pr-10 border-zinc-700 py-6"
+        className="pr-10 border-zinc-700 py-6 rounded-[12px]"
       />
     </div>
   );
