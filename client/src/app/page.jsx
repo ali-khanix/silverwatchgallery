d75c5f150ff904e6f57df423ed1920725621b29f
Ali Khani <khaniali568@gmail.com>
2025-10-12 15:04:41 +0330

    hero image responsive

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative aspect-[1.5/1] h-[70vh]">
+    <div className="relative aspect-[1/1] sm:aspect-[3/1]">
       <Image
         src={"/images/hero-1.webp"}
         alt="عکس ساعت حرفه ای"
