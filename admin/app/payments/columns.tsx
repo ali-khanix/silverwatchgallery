0d21bcf0fa6c61ff85bc08cec43415b2eb5dd946
Ali Khani <103037526+ali-khanix@users.noreply.github.com>
2025-10-31 23:49:37 +0330

    add dropdown to data table

--- admin/app/payments/columns.tsx
+++ admin/app/payments/columns.tsx
@@ -3,6 +3,18 @@
 import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export type Payment = {
   id: string;
   amount: number;
@@ -12,6 +24,34 @@ export type Payment = {
 };
 
 export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>اقدام ها</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              کپی کردن کد پرداخت
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>نمایش مشتری</DropdownMenuItem>
+            <DropdownMenuItem>نمایش جزئیات پرداخت</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
   {
     accessorKey: "username",
     header: "کاربر",
