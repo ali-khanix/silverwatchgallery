a48b677dea7aa68b0a3c5559c2115c796b8fd876
Ali Khani <khaniali568@gmail.com>
2025-11-08 13:22:11 +0330

    Navbar.tsx error size fixed

--- client/components/ShoppingCartIcon.tsx
+++ client/components/ShoppingCartIcon.tsx
@@ -4,14 +4,24 @@ import useCartStore from "@/stores/cartStore";
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
 
-const ShoppingCartIcon = () => {
+type ShoppingCartIconProps = {
+  size?: number;
+  className?: string;
+};
+
+const ShoppingCartIcon = ({ size = 28, className }: ShoppingCartIconProps) => {
   const { cart } = useCartStore();
 
+  const totalItems = cart.reduce((acc, cur) => acc + cur.quantity, 0);
+
   return (
-    <Link href={"/cart"} className="hover:text-zinc-400 relative">
-      <ShoppingCart size={28} />
+    <Link
+      href="/cart"
+      className={`hover:text-zinc-400 relative ${className ?? ""}`}
+    >
+      <ShoppingCart size={size} />
       <span className="absolute -top-3 -right-3 bg-zinc-100 text-zinc-900 rounded-full w-6 h-6 translate-y-1 flex items-center justify-center text-xs font-semibold">
-        {cart.reduce((acc, cur) => acc + cur.quantity, 0)}
+        {totalItems}
       </span>
     </Link>
   );
